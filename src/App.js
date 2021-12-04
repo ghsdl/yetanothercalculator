8787f09b97ea14f4e97c23e2b7cb9aa43070c5c8
ghsdl <maeva.ghsdl@gmail.com>
2021-12-04 17:30:48 +0100

    :art: CSS Style
    CSS Style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-function App() {
+import MainContainer from './components/MainContainer/MainContainer.jsx';
+import ScreenContainer from './components/ScreenContainer/ScreenContainer.jsx';
+import ButtonsContainer from './components/ButtonsContainer/ButtonsContainer.jsx';
+import Button from './components/Button/Button.jsx';
+
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>Welcome to YAT!</p>
-      </header>
-    </div>
+    <MainContainer>
+      <ScreenContainer value='0' />
+      <ButtonsContainer>
+        <Button
+          className=''
+          value='C'
+          onClick={() => {
+            console.log('Je clique sur mon bouton normalement!');
+          }}
+        />
+      </ButtonsContainer>
+    </MainContainer>
   );
-}
+};
 
 export default App;
